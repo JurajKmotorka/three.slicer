1bbc85ec643047b69c95eab270fa7627b8395602
Juraj Kmotorka <jurajkmo@gmail.com>
2024-03-20 01:00:43 +0100

    demo

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,13 @@ import { OrbitControls } from "@react-three/drei";
 import "./App.css";
 import img1 from "/1.png";
 import img2 from "/2.png";
+import img3 from "/3.png";
+import img4 from "/4.png";
+import img5 from "/5.png";
+import img6 from "/6.png";
+import img7 from "/7.png";
+import img8 from "/8.png";
+import { degToRad } from "three/src/math/MathUtils";
 
 function Image({ img, position }) {
   const texture = useLoader(THREE.TextureLoader, img);
@@ -30,8 +37,9 @@ const Scene = () => {
       <pointLight position={[5, 5, 5]} intensity={1} />
       <pointLight position={[-3, -3, 2]} />
       <OrbitControls />
-      <Image img={img1} position={[0, 0, 0]} />
-      <Image img={img2} position={[0, 0, 1]} />
+      {[img1, img2, img3, img4, img5, img6, img7, img8].map((img, i) => (
+        <Image key={i} img={img} position={[0, 0, i]} />
+      ))}
     </>
   );
 };
@@ -39,7 +47,12 @@ const Scene = () => {
 const App = () => {
   return (
     <div id="canvas-container">
-      <Canvas camera={{ fov: 70, position: [0, 0, 3] }}>
+      <Canvas
+        camera={{
+          fov: 70,
+          position: [0, 0, -3],
+        }}
+      >
         <OrbitControls />
         <Scene />
       </Canvas>
